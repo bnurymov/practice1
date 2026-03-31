e03eafafc17d8d1e51d00b4c908936e37993b158
bnurymov <b_nurymov@kbtu.kz>
2026-03-31 23:01:30 +0500

    lab7

--- lab7/config.py
+++
@@ -1,8 +0,0 @@
-from configparser import ConfigParser
-
-def config(filename='database.ini', section='postgresql'):
-    parser = ConfigParser()
-    parser.read(filename)
-    if not parser.has_section(section):
-        raise Exception(f'Section [{section}] not found in {filename}')
-    return dict(parser.items(section))
--- lab7/phonebook.py
+++ lab7/phonebook.py
@@ -1,123 +1,115 @@
 import psycopg2
 import csv
 
-from config import *
+conn = psycopg2.connect(
+    host="localhost",
+    database="mydb",
+    user="nurymoff2",
+    password="ktoto2008"
+)
 
-def print_help():
-    print(r"add {name: string} {phone_number: integer} - add")
-    print(r"edit {choice: (name: string) or (phone_num: integer)} {new_valuer} - edit")
-    print(r"get {choice: (name: string) or (phone_num: integer) - get}")
-    print(r"delete {choice: (name: string) or (phone_num: integer) - delete")
-    print(r"importcsv {file_path} - inserts .csv file to PhoneBook")
+cur = conn.cursor()
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS contacts (
+        name VARCHAR(100),
+        number VARCHAR(100) PRIMARY KEY
+    )
+""")
+conn.commit()
 
-def create_table(cur):
-    cur.execute("CREATE TABLE IF NOT EXISTS contacts (name VARCHAR(50) NOT NULL, phone_number INTEGER UNIQUE NOT NULL);")
-
-def delete_table(cur):
-    cur.execute("DROP TABLE IF EXISTS contacts;")
-
-
-def add_contact(cur, name, phone_number):
-    cmd = f"INSERT INTO contacts (name, phone_number) VALUES ('{name}', {phone_number});"
-    cur.execute(cmd)
-
-def edit_contact(cur, type, change, value):
-    cmd = "UPDATE contacts SET"
-    if type == 0:
-        cmd += f"phone_number = {int(value)} WHERE name = '{change}';"
-    elif type == 1:
-        cmd += f"name = '{value}' WHERE phone_number = {int(change)};"
-    cur.execute(cmd)
-
-def get_contact(cur, x):
-    cmd = f"SELECT * FROM contacts WHERE name = '{x}' OR CAST(phone_number AS TEXT) = '{x}';"
-    cur.execute(cmd)
-    return cur.fetchall()
-
-def delete_contact(cur, x):
-    cmd = f"DELETE FROM contacts WHERE name = '{x}' OR CAST(phone_number AS TEXT) = '{x}';"
-    cur.execute(cmd)
-
-def add_csv_file(cur, path):
-    with open(path, newline='', encoding='utf-8') as f:
-        data = csv.DictReader(f)
-        for r in data:
-            cur.execute("INSERT INTO contacts (name, phone_number) VALUES (%s, %s);", (r['name'], r['phone_number']))
-
-def main():
-    # connect to the database
-    cfg = load_config() # note: this function returns object
-    conn = psycopg2.connect(**cfg)
-    print("Connected to the database")
-    cur = conn.cursor()
-
-    # main code
-
-    run = True
-
-    print(r"Type '?' or 'help' to print help inforamtion")
-    
-    while run:
-        cmd = input()
-
-        if cmd == "":
-            continue
-        cmds = list(cmd.split())
-
-        match cmds[0]:
-            case "quit":
-                run = False
-            case "q":
-                run = False
-            case "exit":
-                run = False
-
-            case "?":
-                print_help()
-            case "help":
-                print_help()
-
-            case "add":
-                if len(cmds) >= 2:
-                    if get_contact(cur, cmds[2]):
-                        print("Phone number already exsists")
-                    else:
-                        add_contact(cur, cmds[1], cmds[2])
-                else:
-                    print("Syntax error")
-            case "edit":
-                pass
-            case "get":
-                found = get_contact(cur, cmds[1])
-
-                if len(cmds) == 1:
-                    print("Syntax error")
-                    continue
-                
-                if len(found) == 0:
-                    print("Not found")
-                else:
-                    print(f"Name: {found[0][0]}")
-                    print(f"Phone number: {found[0][1]}")
-            case "delete":
-                if len(cmds) == 1:
-                    print("Syntax error")
-                    continue
-                delete_contact(cur, cmds[1])
-            case "importcsv":
-                add_csv_file(cur, cmds[1])
-            
+def create():
+    name = input("Name: ")
+    number = input("Number: ")
+    try:
+        cur.execute(
+            "INSERT INTO contacts (name, number) VALUES (%s, %s)",
+            (name, number)
+        )
         conn.commit()
-    # save and close connection
+        print("Contact was added")
+    except psycopg2.Error as e:
+        conn.rollback()
+        print(f"Error: {e}")
+
+def read():
+    aors = input("All or specific contact: ")
+    if(aors == "specific"):
+        norn = input("Name or number: ")
+        if(norn.isdigit() or "+" in norn):
+            cur.execute("SELECT * FROM contacts WHERE number=%s", (norn,))
+            row = cur.fetchall()
+            if not row:
+                print("No contact")
+            else:
+                print(f"Name: {row[0][0]} | Number: {row[0][1]}")
+        else:
+            cur.execute("SELECT * FROM contacts WHERE name=%s", (norn,))
+            row = cur.fetchall()
+            if not row:
+                print("No contact")
+            else:
+                print(f"Name: {row[0][0]} | Number: {row[0][1]}")
+    elif(aors == "all"):
+        cur.execute("SELECT * FROM contacts")
+        rows = cur.fetchall()
+        for row in rows:
+            print(f"Name: {row[0]} | Number: {row[1]}")
+
+def update():
+    norn = input("Name or number: ")
+    if(norn.isdigit() or "+" in norn):
+        new_name = input("New name: ")
+        new_number = input("new number: ")
+        cur.execute(
+            "UPDATE contacts SET name=%s, number=%s WHERE number=%s",
+            (new_name, new_number, number)
+        )
+    else:
+        new_name = input("New name: ")
+        new_number = input("new number: ")
+        cur.execute(
+            "UPDATE contacts SET name=%s, number=%s WHERE name=%s",
+            (new_name, new_number, name)
+        )
     conn.commit()
-    print("Commands saved")
+    print("Contact updated")
 
-    cur.close()
-    conn.close()
-    print("Connection closed")
+def delete():
+    norn = input("Name or number: ")
+    if(norn.isdigit() or "+" in norn):
+        cur.execute("DELETE FROM contacts WHERE number=%s", (norn,))
+    else:
+        cur.execute("DELETE FROM contacts WHERE name=%s", (norn,))
+    conn.commit()
+    print("Contact deleted")
+
+with open("contacts.csv", 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    next(reader)
+    count = 0
+    for row in reader:
+        try:
+            cur.execute(
+                "INSERT INTO contacts (name, number) VALUES (%s, %s) ON CONFLICT DO NOTHING",
+                (row[0], row[1])
+            )
+            count += 1
+        except psycopg2.Error as e:
+            conn.rollback()
+            print(f"Error{row}: {e}")
+    conn.commit()
+
+while True:
+    word = input()
+    if(word == "w"):
+        create()
+    elif(word == "r"):
+        read()
+    elif(word == "u"):
+        update()
+    elif(word == "d"):
+        delete()
+    elif(word == "q"):
+        break
 
 
-try:
-    main()
-except (psycopg2.DatabaseError, Exception) as error:
-    print(error)
